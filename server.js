b88b4176c42a66e0a007a2865fa502d3e552b833
Jimmys Mac <eadesj25@gmail.com>
2023-04-30 13:13:21 -0400

    progress

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ async function main() {
     try {
         const mysql = require('mysql2/promise');
         // create the connection
-        connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'employees_db' });
+        connection = await mysql.createConnection({ 
+            host: 'localhost', 
+            user: 'root', 
+            database: 'employees_db' 
+        });
         console.log("Connection created")
         // query database
         mainMenu()
@@ -22,7 +26,140 @@ async function mainMenu() {
 
     const [departmentData, fields] = await connection.execute("SELECT * FROM department");
 
-    console.table(departmentData)
+    // console.table(departmentData)
+
+//
+
+    const answer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "choices",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit"
+            ]
+        }
+    ])
+    .then((responses) => {
+        const { choices } = responses;
+        console.log(responses)
+
+        if (choices === "View all departments") {
+            viewDepartments();
+        }
+
+        if (choices === "View all roles") {
+            viewRoles()
+        }
+
+        if (choices === "View all employees") {
+            viewEmployees()
+        }
+
+        if (choices === "Add a department") {
+            addDepartment()
+        }
+
+        if (choices ===  "Add a role") {
+            addRole()
+        }
+
+        if (choices === "Add an employee") {
+            addEmployee()
+        }
+
+        if (choices === "Update an employee role") {
+            updateRole()
+        }
+
+        if (choices === "Exit") {
+            process.exit()
+        }
+        
+    })
 }
+
+async function viewDepartments(){
+    const departmentData = await connection.query("select * from department")
+    console.table(departmentData[0])
+    mainMenu()
+};
+
+async function viewEmployees(){
+    const employeesData = await connection.query("select * from employees")
+    console.table(employeesData[0])
+    mainMenu()
+};
+
+async function viewRoles(){
+    const roleData = await connection.query("select * from role")
+    console.table(roleData[0])
+    mainMenu()
+};
+
+async function addDepartment(){
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "departmentName",
+            message: "What's the name of the department you would like to add",
+        }
+    ])
+    console.log(answer)
+    const departmentData = await connection.query("insert into department(name) values (?)", answer.departmentName)
+    console.log (departmentData)
+};
+
+async function addEmployee(){
+    const roleData = await connection.query("select * from role")
+    const employeeData = await connection.query("select * from employees")
+    // console.log(employeeData)
+    // console.log(roleData)
+    const roleChoices = roleData[0].map(({id, title}) => ({name:title, value:id}))
+    const employeeChoices = employeeData[0].map(({id, first_name, last_name,}) => ({name:`${first_name} ${last_name}`, value:id}))
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeFirstName",
+            message: "What's the employees first name you would like to add",
+        },
+        {
+            type: "input",
+            name: "employeeLastName",
+            message: "What's the employees last name you would like to add",
+        },
+        {
+            type: "list",
+            name: "employeeRole",
+            message: "What's the employees role you would like to add",
+            choices: roleChoices
+        },
+        {
+            type: "list",
+            name: "employeeManager",
+            message: "What's the employees manager role you would like to add",
+            choices: employeeChoices
+        }
+    ])
+    // console.log(answer)
+    const newEmployeeData = await connection.query(`insert into employees (first_name, last_name, role_id, manager_id) values ("${answer.employeeFirstName}", "${answer.employeeLastName}", ${answer.employeeRole}, ${answer.employeeManager})`)
+    // console.log(newEmployeeData[0])
+    mainMenu()
+};
+
+async function addRole(){};
+
+async function updateRole(){};
+
+
+
+
 
 main()
